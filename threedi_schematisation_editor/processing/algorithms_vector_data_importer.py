9065f708074f81d9dae7a75dd7ac5f4fbfbc64c2
leendertvanwolfswinkel <leendert.vanwolfswinkel@nelen-schuurmans.nl>
2025-11-13 13:46:38 +0100

    Add output

--- threedi_schematisation_editor/processing/algorithms_vector_data_importer.py
+++ threedi_schematisation_editor/processing/algorithms_vector_data_importer.py
@@ -89,6 +89,14 @@ class BaseImporter(QgsProcessingAlgorithm):
         )
         self.addParameter(target_gpkg)
 
+        # Hidden file output. Not shown in UI, but available in processing model builder
+        self.addOutput(
+            QgsProcessingOutputFile(
+                self.TARGET_GPKG,
+                'Target schematisation database'
+            )
+        )
+
     def get_source_layer_types(self):
         # Default is both line and point, overridden in connection nodes
         return [QgsProcessing.TypeVectorLine, QgsProcessing.TypeVectorPoint]
